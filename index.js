5f280b2137025f6d883ebad98a5a0d631c25635a
SamSvetlik <sam.svetlik@gmail.com>
2022-08-31 16:51:58 -0500

    final

--- index.js
+++ index.js
@@ -1,66 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const comments = require('./data/comments')
-const contacts = require('./data/contacts')
-const products = require('./data/products')
-const vehicles = require('./data/vehicles')
+const comments = require('./routes/comments')
+const contacts = require('./routes/contacts')
+const products = require('./routes/products')
+const vehicles = require('./routes/vehicles')
+let counter = 0
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(bodyParser.json()
 )
+app.use(comments)
+app.use(contacts)
+app.use(products)
+app.use(vehicles)
 // .get routes
-// get all comments
-app.get('/comments', (req, res) => {
-    console.log(comments)
-    res.json(comments)
-})
-// get single comment
-app.get('/comments/:id', (req, res) => {
-    console.log("this requests a comment by ID")
-    console.log(req.params.id)
-    // the .some method returns false if no comment._id matches the requests's id
-    if(!comments.some(comment => comment._id == req.params.id)){
-      res.status(400).json({ msg: `No comment with an ID of ${req.params.id}`})
-      // the .filter method returns the element[s] of the comments array that pass the test
-    } else res.json(comments.filter(comment => comment._id == req.params.id))
-})
-// get contacts
-app.get('/contacts', (req, res) => {
-    res.json(contacts)
-})
-// get single contact
-app.get('/contacts/:id', (req, res) => {
-    console.log("this requests a contact by ID")
-    console.log(req.params.id)
-    if(!contacts.some(contact => contact._id == req.params.id)){
-      res.status(400).json({ msg: `No contact with an ID of ${req.params.id}`})
-    } else res.json(contacts.filter(contact => contact._id == req.params.id))
-})
-// get products
-app.get('/products', (req, res) => {
-    res.json(products)
-})
-// get single product
-app.get('/products/:id', (req, res) => {
-    console.log("this requests a contact by ID")
-    console.log(req.params.id)
-    if(!products.some(product => product._id == req.params.id)){
-      res.status(400).json({ msg: `No product with an ID of ${req.params.id}`})
-    } else res.json(products.filter(product => product._id == req.params.id))
-})
-// get vehicles
-app.get('/vehicles', (req, res) => {
-    res.json(vehicles)
-})
-// get single vehicle
-app.get('/vehicles/:id', (req, res) => {
-    console.log("this requests a vehicle by ID")
-    console.log(req.params.id)
-    if(!vehicles.some(vehicle => vehicle._id == req.params.id)){
-      res.status(400).json({ msg: `No vehicle with an ID of ${req.params.id}`})
-    } else res.json(vehicles.filter(vehicle => vehicle._id == req.params.id))
-})
+
+
+
+
 
 
 app.listen(port, () => {
